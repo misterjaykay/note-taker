f6f6a61571dcda6f67591b93cc4668146e5d69e6
Jonghyun Kang <misterjaykay@gmail.com>
2020-08-18 17:19:59 -0400

    working with post route to post all the notes

--- server.js
+++ server.js
@@ -10,18 +10,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Data
+const database = [];
 
 // Routes
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/notes", function(req, res){
+app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/api/notes", function(req, res){
+app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
+
+    
+
+    // var mydata = JSON.parse(data);
+    // console.log(mydata[0].title);
+    // console.log(mydata[0].note);
+
     // fs.readFile(path.join(__dirname,"/db/db.json"), function(err){
     //     if (err) {
     //         return err;
@@ -29,6 +37,21 @@ app.get("/api/notes", function(req, res){
     // })
     // res.json();
     // // res.sendFile("/db.json");
+});
+
+app.post("/api/notes", function(req, res) {
+    var entry = req.body;
+    console.log("Entered: ", entry);
+    // look for simliar function like appendFile
+    fs.appendFile(path.join(__dirname,"/db/db.json"), entry, function(err) {
+        if (err) throw err;
+        console.log("Checking to see this: ",entry);
+    });
+    
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    res.json
 });
 
 // Listening
