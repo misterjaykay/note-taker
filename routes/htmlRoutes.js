7118d9db521423a1131296fbb64ff6b583b1eb64
Jonghyun Kang <misterjaykay@gmail.com>
2020-08-19 20:59:10 -0400

    added another HTML routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,6 +7,10 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "../public/index.html"));
+      });
+
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     });
