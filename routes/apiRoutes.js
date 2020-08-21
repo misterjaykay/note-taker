a82dd3a31b8fd988340cf443c9e96c2e2e9ec382
Jonghyun Kang <misterjaykay@gmail.com>
2020-08-21 17:21:24 -0400

    changed for loop fucntion to filter and use uuid

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const { v1: uuidv1 } = require('uuid');
 const indexData = require("../db/db.json") // test to use this on GET method
 
 module.exports = function(app) {
@@ -11,12 +12,14 @@ module.exports = function(app) {
     app.post("/api/notes", function(req, res) {
         const data = fs.readFileSync(path.join(__dirname,"../db/db.json"));
         const parseData = JSON.parse(data);
+        
+        const { title, text } = req.body;
+        const newNote = {title, text, id:uuidv1()}
+        parseData.push(newNote); // pushing
 
-        var entry = req.body;
-        parseData.push(entry);
-        for (var i = 0; i < parseData.length; i++) {
-            parseData[i].id = i + 1;   
-        }
+        // for (var i = 0; i < parseData.length; i++) {
+        //     parseData[i].id = i++;
+        // }
         
         const output = JSON.stringify(parseData, null, 2);
 
@@ -24,26 +27,31 @@ module.exports = function(app) {
             if (err) throw err;
         });
 
-        res.json(output);    
+        res.json(output).status(201);
     });
 
     app.delete("/api/notes/:id", function(req, res) {
         const data = fs.readFileSync(path.join(__dirname,"../db/db.json"));
         const parseData = JSON.parse(data);
+        console.log("param id",req.params.id);
         
-        var idNumb = req.params.id;
-        parseData.splice(idNumb, 1);
-        for (var i = 0; i < parseData.length; i++) {
-            parseData[i].id = i + 1;   
-        }
+        console.log("whatsthis",parseData);
+        const filterData = parseData.filter((notes) => notes.id !== req.params.id);
+        console.log("filter", filterData);
+        // var idNumb = req.params.id;
+        // parseData.splice(idNumb, 1);
 
-        const output = JSON.stringify(parseData, null, 2);
+        // for (var i = 0; i < parseData.length; i++) {
+        //     parseData[i].id = i + 1;   
+        // }
+
+        const output = JSON.stringify(filterData, null, 2);
 
         fs.writeFile(path.join(__dirname, "../db/db.json"), output, function(err) {
             if (err) throw err;
         });
         
-        res.json(output);
+        res.status(200).json(output);
     });
 };
 /// Ref to clear all
